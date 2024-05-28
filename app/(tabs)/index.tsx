aeab28e2caf56e96cabf37230050a5b783eff776
Gabriel Lopes <gabrielrrlopes@gmail.com>
2024-05-27 21:08:45 -0400

    Fixed home screen newest content flex styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,20 +14,39 @@ export default function TabOneScreen() {
           <Text style={styles.newestTitle}>Count</Text>
         </View>
 
-        <View style={styles.newestContentContainer}>
-          <View style={styles.newestContentDate}>
-            <Text>Wed</Text>
-            <Text>8</Text>
-            <Text>MAY</Text>
+        <View style={styles.newestMainContentContainer}>
+          <View style={styles.newestContentContainer}>
+            <View style={styles.newestContentDate}>
+              <Text>Wed</Text>
+              <Text>8</Text>
+              <Text>MAY</Text>
+            </View>
+            <View>
+              <Text>11:00 a.m - 06.00 p.m</Text>
+              <Text>code</Text>
+              <Text>creator</Text>
+            </View>
+            <View style={styles.newestContentQty}>
+              <Text>10</Text>
+            </View>
           </View>
-          <View style={styles.newestContent}>
-            <Text>11:00 a.m - 6.00 p.m</Text>
-            <Text>code</Text>
-            <Text>creator</Text>
+
+          <View style={styles.newestContentContainer}>
+            <View style={styles.newestContentDate}>
+              <Text>Tue</Text>
+              <Text>25</Text>
+              <Text>Sept</Text>
+            </View>
+            <View>
+              <Text>08:40 a.m - 03.00 p.m</Text>
+              <Text>code</Text>
+              <Text>creator</Text>
+            </View>
+            <View style={styles.newestContentQty}>
+              <Text>98</Text>
+            </View>
           </View>
-          <View style={styles.newestContentQty}>
-            <Text>10</Text>
-          </View>
+
         </View>
       </ScrollView>
     </SafeAreaView >
@@ -55,6 +74,11 @@ const styles = StyleSheet.create({
   newestContentContainer: {
     flexDirection: "row",
     columnGap: 26,
+    paddingBottom: 20,
+  },
+  newestMainContentContainer: {
+    flex: 1,
+    flexDirection: "column",
   },
   labelContainer: {
     flex: 1,
@@ -69,6 +93,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingLeft: 40
   },
-  newestContent: {
-  }
 });
